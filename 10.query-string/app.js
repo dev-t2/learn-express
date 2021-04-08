24810102f46bb31d4cf11ee689ca22c1c8472281
t2 <oo_ri@naver.com>
2021-04-09 01:54:30 +0900

    Create PORT, BASE URL Constant

--- 10.query-string/app.js
+++ 10.query-string/app.js
@@ -1,12 +1,17 @@
 const http = require('http');
 
-const BASE_URL = 'http://localhost:3000';
+const PORT = 3000;
+const BASE_URL = `http://localhost:${PORT}`;
 
 const app = http.createServer((req, res) => {
   const url = new URL(req.url, BASE_URL);
+
+  if (url.pathname === '/favicon.ico') {
+    return res.writeHead(404);
+  }
 
   res.writeHead(200);
   res.end(url.searchParams.get('id'));
 });
 
-app.listen(3000);
+app.listen(PORT);
