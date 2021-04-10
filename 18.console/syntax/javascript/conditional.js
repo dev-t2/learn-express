cb8226d093baac63286e0718aad520a40aeab9a9
t2 <oo_ri@naver.com>
2021-04-10 16:22:59 +0900

    Console Input Update

--- 18.console/syntax/javascript/conditional.js
+++ 18.console/syntax/javascript/conditional.js
@@ -1,9 +1,9 @@
-const condition = process.argv[2];
+const argv = process.argv;
 
 console.log('A');
 console.log('B');
 
-if (condition === '1') {
+if (argv[2] === '1') {
   console.log('C1');
 } else {
   console.log('C2');
