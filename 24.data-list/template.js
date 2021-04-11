c3fc3228a901cb6cfe52b1b41dffabe09a5c6d7e
t2 <oo_ri@naver.com>
2021-04-11 17:49:12 +0900

    Create 24.data-list Folder and app.js Refactoring

---
+++ 24.data-list/template.js
@@ -0,0 +1,36 @@
+const template = ({ title, list, description }) => `
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>${title}</title>
+  </head>
+
+  <body>
+    <h1>
+      <a href="/">Web</a>
+    </h1>
+
+    <ul>
+      ${list
+        .map(li => {
+          const name = li.split('.')[0];
+
+          return `<li><a href="/?id=${name}">${name}</a></li>`;
+        })
+        .join('')}
+    </ul>
+
+    <h2>${title}</h2>
+
+    <p>
+      ${description}
+    </p>
+  </body>
+</html>
+`;
+
+module.exports = template;
