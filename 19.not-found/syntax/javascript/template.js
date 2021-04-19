4bdd8cd9d31316ed5bf845409342297274f2d3b6
t2 <oo_ri@naver.com>
2021-04-19 18:13:39 +0900

    Create 19.not-found Folder

---
+++ 19.not-found/syntax/javascript/template.js
@@ -0,0 +1,15 @@
+const language = 'JavaScript';
+
+const lorem = `Sit sint cillum ${language} voluptate ullamco.Ipsum pariatur ullamco aute fugiat elit eiusmod mollit.
+Fugiat do occaecat esse occaecat dolore in amet nisi deserunt cillum enim.
+Veniam officia aliqua exercitation velit deserunt reprehenderit quis irure cillum laborum.
+Eu proident sit ea aliquip esse dolore reprehenderit pariatur excepteur. Eiusmod sunt do quis quis aliquip.
+Ad voluptate anim amet et pariatur sit. Duis exercitation ea sunt elit ea fugiat tempor consectetur eiusmod non qui.
+Pariatur quis sint irure velit culpa ${language} voluptate nisi ea sunt commodo aliquip.
+Sint in ut cupidatat non esse officia proident. Cupidatat in nisi tempor ${language} est minim ut est deserunt dolore ad nulla enim non.
+Proident anim consequat laborum nostrud duis enim mollit non ipsum duis nisi labore occaecat.
+Esse sint consectetur sint irure ${language} est anim veniam consectetur officia ad sint. Enim irure do ut labore anim mollit fugiat commodo.
+Nisi esse est occaecat elit aliqua aute pariatur. Quis aliquip excepteur sunt eiusmod aliquip nisi enim culpa id reprehenderit voluptate ea.
+Aliquip nostrud enim tempor aliquip laboris commodo magna.`;
+
+console.log(lorem);
