56df19f5e819fe48e78e1162568e70bcffc73282
t2 <oo_ri@naver.com>
2021-10-29 19:37:20 +0900

    14_funtion.ts and 15_condition.ts Update

--- 01_typescript/01_basic/14_function.ts
+++ 01_typescript/01_basic/14_function.ts
@@ -13,3 +13,9 @@ console.log(add2(1, 2));
 const add3 = (a: number, b: number) => a + b;
 
 console.log(add3(1, 2));
+
+const func = (...args: number[]) => {
+  console.log(args);
+};
+
+func(1, 2, 3, 4);
