a16b869f95677fba0ff30bf6c262f224f83ad56e
t2 <oo_ri@naver.com>
2021-10-29 19:31:49 +0900

    01_basic Folder Update

---
+++ 01_typescript/01_basic/08_string.ts
@@ -0,0 +1,5 @@
+const a = 'Hello';
+const b = 'JavaScript';
+
+console.log(a + ' ' + b);
+console.log(`${a} ${b}`);
---
+++ 01_typescript/01_basic/16_condition.ts
@@ -0,0 +1,15 @@
+const a = 1;
+
+if (a + 1 === 2) {
+  console.log('a + 1은 2와 같다.');
+}
+
+const c = 10;
+
+if (c > 15) {
+  console.log('c가 15보다 크다.');
+} else if (c === 10) {
+  console.log('c는 10이다.');
+} else {
+  console.log('c가 15보다 작다.');
+}
