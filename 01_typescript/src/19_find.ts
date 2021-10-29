2c217fd9bea8ad5fe7fece5a8f107b97a7e2c4be
t2 <oo_ri@naver.com>
2021-10-29 22:02:08 +0900

    19_find.ts Update

---
+++ 01_typescript/src/19_find.ts
@@ -0,0 +1,18 @@
+const todos = [
+  { id: 1, text: 'JavaScript', isDone: true },
+  { id: 2, text: 'NodeJS', isDone: true },
+  { id: 3, text: 'TypeScript', isDone: true },
+  { id: 4, text: 'Express', isDone: false },
+];
+
+const index = todos.findIndex((todo) => {
+  return todo.id === 3;
+});
+
+console.log(index);
+
+const todo = todos.find((todo) => {
+  return todo.id === 3;
+});
+
+console.log(todo);
