a83b55127e0cc57634d50fecfcb99070679c6740
t2 <oo_ri@naver.com>
2021-10-29 22:23:50 +0900

    23_short-circuit.ts Update

---
+++ 01_typescript/src/23_short-circuit.ts
@@ -0,0 +1,21 @@
+console.log(true && 'hello');
+console.log(false && 'hello');
+console.log('hello' && 'bye');
+console.log(null && 'hello');
+console.log(undefined && 'hello');
+console.log('' && 'hello');
+console.log(0 && 'hello');
+console.log(1 && 'hello');
+console.log(1 && 1);
+
+console.log('');
+
+console.log(true || 'hello');
+console.log(false || 'hello');
+console.log('hello' || 'bye');
+console.log(null || 'hello');
+console.log(undefined || 'hello');
+console.log('' || 'hello');
+console.log(0 || 'hello');
+console.log(1 || 'hello');
+console.log(1 || 1);
