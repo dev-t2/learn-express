a33d91635ca89adcd63fff2b53e06d517e987e35
t2 <oo_ri@naver.com>
2021-10-29 19:21:36 +0900

    01_basic Folder Update

---
+++ 01_typescript/01_basic/07_comparison.ts
@@ -0,0 +1,14 @@
+const a = 1;
+const b = 1;
+
+console.log(a === b);
+console.log(a !== b);
+
+const c = 10;
+const d = 15;
+const e = 15;
+
+console.log(c < d);
+console.log(c > d);
+console.log(d >= e);
+console.log(c <= e);
