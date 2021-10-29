c01f515c7d9cda9a67d918b613db13ca23bab31a
t2 <oo_ri@naver.com>
2021-10-29 17:45:38 +0900

    01_basic Folder Update

---
+++ 01_typescript/01_basic/04_type.ts
@@ -0,0 +1,19 @@
+const a = 1;
+
+console.log(a);
+
+const b = 'hello';
+
+console.log(b);
+
+const c = false;
+
+console.log(c);
+
+const d = undefined;
+
+console.log(d);
+
+const e = null;
+
+console.log(e);
