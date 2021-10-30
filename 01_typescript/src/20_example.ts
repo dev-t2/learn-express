825f974b056ef4e7b88037daa0cf989d6598d30f
t2 <oo_ri@naver.com>
2021-10-30 13:06:30 +0900

    20_example.ts Update

--- 01_typescript/src/20_example.ts
+++ 01_typescript/src/20_example.ts
@@ -1,15 +1,7 @@
-const countBiggerThanTen = (numbers: number[]) => {
-  const result = numbers.reduce((count, number) => {
-    if (number > 10) {
-      return count + 1;
-    }
-
-    return count;
+const sum = (...rest: number[]) => {
+  return rest.reduce((sum, number) => {
+    return sum + number;
   }, 0);
-
-  return result;
 };
 
-const result = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
-
-console.log(result);
+console.log(sum(1, 2, 3, 4, 5));
