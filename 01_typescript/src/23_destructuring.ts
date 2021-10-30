76082a10a73c00bfe609b09d5bed89ccc47ab36a
t2 <oo_ri@naver.com>
2021-10-30 12:26:38 +0900

    23_destructuring.ts Update

--- 01_typescript/src/23_destructuring.ts
+++ 01_typescript/src/23_destructuring.ts
@@ -1,14 +1,16 @@
 const array = [1, 2];
 
-const [first, second] = array;
+const [first, second, third = 3] = array;
 
 console.log(first);
 console.log(second);
+console.log(third);
 
 console.log();
 
-const object = { a: 1, b: 2 };
-const { a, b = 2 } = object;
+const object = { a: 1, b: 2, c: 3 };
+const { a, b = 2, c: d } = object;
 
 console.log(a);
 console.log(b);
+console.log(d);
