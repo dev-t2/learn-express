d118dccf44f473b08656febc5fce469a15c92333
t2 <oo_ri@naver.com>
2021-10-30 16:18:32 +0900

    30_promise.ts Update

---
+++ 01_typescript/src/30_promise.ts
@@ -0,0 +1,24 @@
+type Resolve = (date: string) => void;
+
+const work = (ms: number) => {
+  return new Promise((resolve: Resolve, reject) => {
+    setTimeout(() => {
+      resolve(new Date().toISOString());
+    }, ms);
+  });
+};
+
+work(1000)
+  .then((date) => {
+    console.log(`첫 번째 작업: ${date}`);
+
+    return work(1000);
+  })
+  .then((date) => {
+    console.log(`두 번째 작업: ${date}`);
+
+    return work(1000);
+  })
+  .then((date) => {
+    console.log(`세 번째 작업: ${date}`);
+  });
