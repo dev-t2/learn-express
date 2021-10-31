b85e000502d2196bb9b94cc38904fbfbfed7ffe9
t2 <oo_ri@naver.com>
2021-10-31 16:17:33 +0900

    Not Found and Internal Server Error Middleware Update

--- 05_middleware/src/app.ts
+++ 05_middleware/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 const port = 8080;
@@ -11,6 +11,17 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
+});
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
