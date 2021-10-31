e61a9220ddd6ff0b0a43cf404e536795f991e43a
t2 <oo_ri@naver.com>
2021-10-31 12:29:26 +0900

    03_http Public Folder Delete

--- 03_http/src/app.ts
+++ 03_http/src/app.ts
@@ -1,22 +1,11 @@
 import http from 'http';
-import fs from 'fs/promises';
 
 const port = 8080;
 
 const app = http.createServer(async (req, res) => {
-  // res.end('Hello NodeJS');
-
   console.log({ url: req.url, method: req.method });
 
-  try {
-    const file = await fs.readFile(`${__dirname}/public/index.html`);
-
-    res.statusCode = 200;
-    res.end(file);
-  } catch (e) {
-    res.statusCode = 500;
-    res.end((e as Error).message);
-  }
+  res.end('Hello NodeJS');
 });
 
 app.listen(port, () => {
