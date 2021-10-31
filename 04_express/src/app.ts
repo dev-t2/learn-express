5f1eff2cdb17ebfd1f3df7893bb52eb29825a4c2
t2 <oo_ri@naver.com>
2021-10-31 16:31:56 +0900

    app.ts Update

--- 04_express/src/app.ts
+++ 04_express/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
 const app = express();
-const port = 8080;
+
+app.set('port', 8080);
 
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(app.get('port'), () => {
+  console.log(`Server running at http://localhost:${app.get('port')}/`);
 });
