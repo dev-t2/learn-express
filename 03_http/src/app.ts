0eb81ac3f518dc731390987ab474ef1cbcf6fbee
t2 <oo_ri@naver.com>
2021-10-31 12:08:14 +0900

    03_http Complete

--- 03_http/src/app.ts
+++ 03_http/src/app.ts
@@ -1,11 +1,22 @@
 import http from 'http';
+import fs from 'fs/promises';
 
 const port = 8080;
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  // res.end('Hello NodeJS');
+
   console.log({ url: req.url, method: req.method });
 
-  res.end('Hello NodeJS');
+  try {
+    const file = await fs.readFile(`${__dirname}/public/index.html`);
+
+    res.statusCode = 200;
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.end((e as Error).message);
+  }
 });
 
 app.listen(port, () => {
