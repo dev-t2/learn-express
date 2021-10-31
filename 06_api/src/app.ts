7f7dd6828999b972a78c572328c6c90663d8c846
t2 <oo_ri@naver.com>
2021-10-31 22:18:10 +0900

    06_api Complete

--- 06_api/src/app.ts
+++ 06_api/src/app.ts
@@ -1,14 +1,56 @@
 import express, { NextFunction, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
+import axios from 'axios';
+
+dotenv.config();
 
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+const { NODE_ENV, PORT, SERVICE_KEY } = process.env;
 
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
+});
+
+app.get('/air/:location', async (req, res, next) => {
+  const { location } = req.params;
+
+  const encodedLocation = encodeURI(location);
+  const endPoint = `http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=${SERVICE_KEY}`;
+  const url = `${endPoint}&returnType=json&numOfRows=1&pageNo=1&stationName=${encodedLocation}&dataTerm=DAILY&ver=1.3`;
+
+  try {
+    const { data } = await axios.get(url);
+    const { dataTime, pm10Value, pm25Value } = data.response.body.items[0];
+    const status = { pm10: '', pm20: '' };
+
+    if (pm10Value <= 30) {
+      status.pm10 = '좋음😀';
+    } else if (pm10Value > 30 && pm10Value <= 80) {
+      status.pm10 = '보통😐';
+    } else {
+      status.pm10 = '나쁨😱';
+    }
+
+    if (pm25Value <= 15) {
+      status.pm20 = '좋음😀';
+    } else if (pm25Value > 15 && pm25Value <= 35) {
+      status.pm20 = '보통😐';
+    } else {
+      status.pm20 = '나쁨😱';
+    }
+
+    res.send(
+      `관측 지역: ${location} / 관측 시간: ${dataTime} / 미세 먼지: ${status.pm10} / 초미세 먼지: ${status.pm20}`
+    );
+  } catch (err) {
+    next(err);
+  }
 });
 
 app.use((req, res) => {
@@ -22,6 +64,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send('Internal Server Error');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Server running at http://localhost:${app.get('port')}/`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
