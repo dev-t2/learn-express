d38bb7ecf39e0be9371b9dcccbc3215184cc0634
t2 <oo_ri@naver.com>
2021-10-31 16:43:08 +0900

    Static Middleware Update

--- 05_middleware/src/app.ts
+++ 05_middleware/src/app.ts
@@ -1,11 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
 
 const app = express();
 
 app.set('port', process.env.PORT || 8080);
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello NodeJS');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.use((req, res) => {
