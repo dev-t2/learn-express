d590fb35987e97b37e711e15a8d7a8a968d1fccb
t2 <oo_ri@naver.com>
2021-10-31 15:43:02 +0900

    app.ts middleware Update

--- 05_middleware/src/app.ts
+++ 05_middleware/src/app.ts
@@ -3,6 +3,12 @@ import express from 'express';
 const app = express();
 const port = 8080;
 
+app.use((req, res, next) => {
+  console.log('middleware');
+
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
 });
