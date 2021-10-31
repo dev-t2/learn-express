4dd9ecd33452c0128253d713327fd2c442651e35
t2 <oo_ri@naver.com>
2021-10-31 16:54:38 +0900

    Router Middleware Update

--- 05_middleware/src/app.ts
+++ 05_middleware/src/app.ts
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/user/:id', (req, res) => {
+  res.send(`${req.params.id}님의 개인 페이지입니다.`);
+});
+
 app.use((req, res) => {
   res.status(404).send('Not Found');
 });
