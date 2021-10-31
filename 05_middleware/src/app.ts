5cf3a0723e9118523fbc467be3bfc155f49e822d
t2 <oo_ri@naver.com>
2021-10-31 15:31:37 +0900

    Express Install and Setup

---
+++ 05_middleware/src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello NodeJS');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
