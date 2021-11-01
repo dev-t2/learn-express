20725534c0e405f7095cceec1d7165bdad80c13c
t2 <oo_ri@naver.com>
2021-11-02 00:00:12 +0900

    Redis Install and Setup

--- 06_redis/src/app.ts
+++ 06_redis/src/app.ts
@@ -1,27 +1,10 @@
-import express, { NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
+import redis from 'redis';
 
-const app = express();
+const client = redis.createClient();
 
-app.set('port', process.env.PORT || 8080);
-
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-
-app.get('/', (req, res) => {
-  res.send('Hello NodeJS');
+client.on('error', (error: Error) => {
+  console.error(error);
 });
 
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
-
-// eslint-disable-next-line no-unused-vars
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-
-  res.status(500).send('Internal Server Error');
-});
-
-app.listen(app.get('port'), () => {
-  console.log(`Server running at http://localhost:${app.get('port')}/`);
-});
+client.set('key', 'value', redis.print);
+client.get('key', redis.print);
