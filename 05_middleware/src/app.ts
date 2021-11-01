c21a7c5b4e425da836daf24e09b192ea3efa5a59
t2 <oo_ri@naver.com>
2021-11-01 23:39:11 +0900

    Static Middleware Delete

--- 05_middleware/src/app.ts
+++ 05_middleware/src/app.ts
@@ -1,16 +1,14 @@
 import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
-import path from 'path';
 
 const app = express();
 
 app.set('port', process.env.PORT || 8080);
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('Hello NodeJS');
 });
 
 app.use((req, res) => {
