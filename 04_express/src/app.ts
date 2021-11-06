f4cd8d707f6b9d2514c5a969ff357d4833dfce22
t2 <oo_ri@naver.com>
2021-11-06 11:06:41 +0900

    04_express Complete

--- 04_express/src/app.ts
+++ 04_express/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.set('port', process.env.PORT || 8080);
+app.set('port', 8080);
 
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
