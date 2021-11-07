ea71e720791a04df87d821a4d2a918e1b5ef386b
t2 <oo_ri@naver.com>
2021-11-07 23:55:40 +0900

    04_express Complete

--- 04_express/src/app.ts
+++ 04_express/src/app.ts
@@ -1,13 +1,27 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
-app.set('port', 8080);
+const port = 3000;
+
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.get('/', (req, res) => {
   res.send('Hello NodeJS');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`Server running at http://localhost:${app.get('port')}/`);
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
