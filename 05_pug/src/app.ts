7bd1496e9daaa238a807390d8e80f7737407966c
t2 <oo_ri@naver.com>
2021-11-09 00:05:37 +0900

    Pug Install and Setup

--- 05_pug/src/app.ts
+++ 05_pug/src/app.ts
@@ -1,25 +1,28 @@
 import express, { NextFunction, Request, Response } from 'express';
+import path from 'path';
 import morgan from 'morgan';
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello NodeJS');
+  res.render('index');
 });
 
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  res.status(404).render('404');
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
-  res.status(500).send('Internal Server Error');
+  res.status(500).render('500');
 });
 
 app.listen(app.get('port'), () => {
