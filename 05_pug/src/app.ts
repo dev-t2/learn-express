14b89a09a202582cc63e81d789308ae964972bc3
t2 <oo_ri@naver.com>
2021-11-09 21:18:23 +0900

    Pug Container Layout and Contents Update

--- 05_pug/src/app.ts
+++ 05_pug/src/app.ts
@@ -12,18 +12,24 @@ app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Hello NodeJS' });
+  res.render('index', { title: 'home', content: 'Hello NodeJS' });
 });
 
 app.use((req, res) => {
-  res.status(404).render('404');
+  res.status(404).render('index', {
+    title: '404 - Not Fount',
+    content: 'Not Found',
+  });
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
-  res.status(500).render('500');
+  res.status(500).render('index', {
+    title: '500 - Internal Server Error',
+    content: 'Internal Server Error',
+  });
 });
 
 app.listen(app.get('port'), () => {
