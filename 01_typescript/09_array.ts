624d0b9cd56257b0e8bb887d2b0c91e205499a99
t2 <oo_ri@naver.com>
2021-11-14 12:13:29 +0900

    Array Built In Function Update

--- 01_typescript/09_array.ts
+++ 01_typescript/09_array.ts
@@ -1,11 +1,34 @@
-const coffee: string[] = [];
+const array = [10, 20, 30, 40];
 
-coffee.push('아메리카노');
-coffee.push('라떼');
+console.log('===== Read and Search Element =====');
+console.log(array);
+console.log(array[1]);
+console.log(array.length);
+console.log(array.indexOf(20));
+console.log(array.includes(30));
+console.log();
 
-console.log(coffee);
-console.log(coffee[0]);
-console.log(coffee.length);
+console.log('===== Left Insert and Delete =====');
+console.log(array.unshift(5));
+console.log(array);
+console.log(array.shift());
+console.log(array);
+console.log();
 
-console.log(coffee.indexOf('라떼'));
-console.log(coffee.includes('아메리카노'));
+console.log('===== Right Insert and Delete =====');
+console.log(array.push(50));
+console.log(array);
+console.log(array.pop());
+console.log(array);
+console.log();
+
+console.log('===== Delete Element =====');
+console.log(array.slice(1, 2));
+console.log(array);
+console.log(array.splice(1, 1));
+console.log(array);
+console.log();
+
+console.log('===== Change Array to String =====');
+console.log(array.join());
+console.log(array.join(' '));
