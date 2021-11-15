fc1278583ed75a8a512b4ea12a10bd6b10cb1945
t2 <oo_ri@naver.com>
2021-11-16 00:22:15 +0900

    09_array.ts Update

--- 01_typescript/09_array.ts
+++ 01_typescript/09_array.ts
@@ -1,6 +1,5 @@
 const array = [10, 20, 30, 40];
 
-console.log('===== Read and Search Element =====');
 console.log(array);
 console.log(array[1]);
 console.log(array.length);
@@ -9,27 +8,23 @@ console.log(array.indexOf(20));
 console.log(array.includes(30));
 console.log();
 
-console.log('===== Left Insert and Delete =====');
 console.log(array.unshift(5));
 console.log(array);
 console.log(array.shift());
 console.log(array);
 console.log();
 
-console.log('===== Right Insert and Delete =====');
 console.log(array.push(50));
 console.log(array);
 console.log(array.pop());
 console.log(array);
 console.log();
 
-console.log('===== Insert and Delete =====');
 console.log(array.splice(2, 0, 25));
 console.log(array);
 console.log(array.splice(2, 1));
 console.log(array);
 console.log();
 
-console.log('===== Change Array to String =====');
 console.log(array.join());
 console.log(array.join(' '));
