2f37e6db2fd95cf6a22c35b5b38a741d6f9a5bc1
t2 <oo_ri@naver.com>
2021-11-16 23:41:04 +0900

    14_for.ts Update

--- 01_typescript/14_for.ts
+++ 01_typescript/14_for.ts
@@ -6,6 +6,26 @@ for (let i = 0; i < names.length; i++) {
 
 console.log();
 
+for (let name of names) {
+  console.log(name);
+}
+
+console.log();
+
 names.forEach((name) => {
   console.log(name);
 });
+
+console.log();
+
+for (let i = 0; i < 10; i++) {
+  if (i === 2) {
+    continue;
+  }
+
+  console.log(i);
+
+  if (i === 5) {
+    break;
+  }
+}
