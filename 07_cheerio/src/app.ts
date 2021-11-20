e66c7b3610003a42c2afad71c31ef0d511732d48
t2 <oo_ri@naver.com>
2021-11-21 00:35:25 +0900

    07_cheerio Update

--- 07_cheerio/src/app.ts
+++ 07_cheerio/src/app.ts
@@ -1,21 +1,34 @@
-import xlsx from 'xlsx';
-import path from 'path';
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-interface Item {
-  [key: string]: string;
-}
+const movies = [
+  {
+    title: '장르만 로맨스',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=185264',
+  },
+  {
+    title: '이터널스',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=184311',
+  },
+  {
+    title: '프렌치 디스패치',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=187312',
+  },
+  {
+    title: '너에게 가는 길',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=203715',
+  },
+  {
+    title: '듄',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=191559',
+  },
+];
 
-const workbook = xlsx.readFile(path.join(__dirname, 'data.xlsx'));
-const sheet = workbook.Sheets['영화 목록'];
-const items: Item[] = xlsx.utils.sheet_to_json(sheet);
-
-items.forEach(async (item) => {
-  const { data } = await axios.get(item['링크']);
+movies.forEach(async (movie) => {
+  const { data } = await axios.get(movie.link);
 
   const $ = cheerio.load(data);
   const score = $('.score.score_left > .star_score').text().trim();
 
-  console.log(`영화: ${item['영화']} / 평점: ${score}`);
+  console.log(`영화: ${movie.title} / 평점: ${score}`);
 });
