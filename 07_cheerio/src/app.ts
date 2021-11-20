e35837aa548a6059c8151b99a9678b53395aedb3
t2 <oo_ri@naver.com>
2021-11-20 22:41:55 +0900

    07_cheerio Update

--- 07_cheerio/src/app.ts
+++ 07_cheerio/src/app.ts
@@ -1,8 +1,27 @@
 import xlsx from 'xlsx';
 import path from 'path';
+import axios from 'axios';
+import cheerio from 'cheerio';
 
-const workbook = xlsx.readFile(path.join(__dirname, 'data.xlsx'));
-const sheet = workbook.Sheets['영화 목록'];
-const data = xlsx.utils.sheet_to_json(sheet);
+interface Item {
+  [key: string]: string;
+}
 
-console.log(data);
+const app = async () => {
+  const workbook = xlsx.readFile(path.join(__dirname, 'data.xlsx'));
+  const sheet = workbook.Sheets['영화 목록'];
+  const items: Item[] = xlsx.utils.sheet_to_json(sheet);
+
+  await Promise.all(
+    items.map(async (item) => {
+      const { data } = await axios.get(item['링크']);
+
+      const $ = cheerio.load(data);
+      const score = $('.score.score_left > .star_score').text().trim();
+
+      console.log(`영화: ${item['영화']}, 평점: ${score}`);
+    })
+  );
+};
+
+app();
