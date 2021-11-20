05784549663eeb891f2d0d5511e4b269d443db26
t2 <oo_ri@naver.com>
2021-11-20 22:13:39 +0900

    07_cheerio Update

--- 07_cheerio/src/xlsx.ts
+++ 07_cheerio/src/xlsx.ts
@@ -1,12 +1,8 @@
 import xlsx from 'xlsx';
 import path from 'path';
 
-const app = () => {
-  const workbook = xlsx.readFile(path.join(__dirname, 'file', 'data.xlsx'));
-  const sheet = workbook.Sheets['영화 목록'];
-  const data = xlsx.utils.sheet_to_json(sheet);
+const workbook = xlsx.readFile(path.join(__dirname, 'data.xlsx'));
+const sheet = workbook.Sheets['영화 목록'];
+const data = xlsx.utils.sheet_to_json(sheet);
 
-  console.log(data);
-};
-
-app();
+console.log(data);
