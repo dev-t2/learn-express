5013931e40071f015583dc9b64025a42c9142710
t2 <oo_ri@naver.com>
2021-11-21 10:39:37 +0900

    Puppeteer Promise Update

--- 08_puppeteer/src/app.ts
+++ 08_puppeteer/src/app.ts
@@ -1,12 +1,14 @@
 import puppeteer from 'puppeteer';
 
 const app = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({
+    headless: process.env.NODE_ENV === 'production',
+  });
+  const [page] = await Promise.all([browser.newPage()]);
 
-  await page.goto('https://www.naver.com');
-  await page.waitForTimeout(5000);
-  await page.close();
+  await Promise.all([page.goto('https://www.naver.com')]);
+  await Promise.all([page.waitForTimeout(5000)]);
+  await Promise.all([page.close()]);
 
   await browser.close();
 };
