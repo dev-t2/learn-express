7f86d0861b8551b73e100efe4965b6929ba7cbd2
t2 <oo_ri@naver.com>
2021-11-21 11:08:52 +0900

    08_puppeteet Update

--- 08_puppeteer/src/app.ts
+++ 08_puppeteer/src/app.ts
@@ -1,16 +1,56 @@
 import puppeteer from 'puppeteer';
 
+const movies = [
+  {
+    title: '장르만 로맨스',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=185264',
+  },
+  {
+    title: '이터널스',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=184311',
+  },
+  {
+    title: '프렌치 디스패치',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=187312',
+  },
+  {
+    title: '너에게 가는 길',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=203715',
+  },
+  {
+    title: '듄',
+    link: 'https://movie.naver.com/movie/bi/mi/basic.naver?code=191559',
+  },
+];
+
 const app = async () => {
+  console.time('dt');
+
   const browser = await puppeteer.launch({
     headless: process.env.NODE_ENV === 'production',
   });
-  const [page] = await Promise.all([browser.newPage()]);
 
-  await Promise.all([page.goto('https://www.naver.com')]);
-  await Promise.all([page.waitForTimeout(5000)]);
-  await Promise.all([page.close()]);
+  const promises = movies.map(async (movie) => {
+    const page = await browser.newPage();
+
+    await page.goto(movie.link);
+
+    const handle = await page.$('.score.score_left > .star_score');
+    const score = await page.evaluate((element) => element.innerText, handle);
+
+    await page.waitForTimeout(5000);
+    await page.close();
+
+    return { title: movie.title, score: parseFloat(score) };
+  });
+
+  const result = await Promise.all(promises);
 
   await browser.close();
+
+  console.log(result);
+  console.log();
+  console.timeEnd('dt');
 };
 
 app();
