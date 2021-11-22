663ea44dad395197f5a9d15f9eef0d1c8efe86ed
t2 <oo_ri@naver.com>
2021-11-23 00:07:10 +0900

    22_reduce.ts Update

--- 01_typescript/22_reduce.ts
+++ 01_typescript/22_reduce.ts
@@ -1,7 +1,17 @@
 const numbers = [1, 2, 3, 4, 5];
 
-const result = numbers.reduce((sum, number) => {
+const sum = numbers.reduce((sum, number) => {
   return sum + number;
 }, 0);
 
-console.log(result);
+console.log(sum);
+
+const average = numbers.reduce((sum, number, index) => {
+  if (index === numbers.length - 1) {
+    return (sum + number) / numbers.length;
+  }
+
+  return sum + number;
+}, 0);
+
+console.log(average);
