09865648a2724dd88c03c9fa14eb0577fa743282
t2 <oo_ri@naver.com>
2021-11-22 23:44:41 +0900

    20_map.ts Update

--- 01_typescript/20_map.ts
+++ 01_typescript/20_map.ts
@@ -1,7 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const result = numbers.map((number) => {
-  return number * number;
+  return number + 1;
 });
 
+console.log(numbers);
 console.log(result);
