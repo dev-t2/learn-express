77914fe5fee1f7f39854c1d7492b93086bc27870
t2 <oo_ri@naver.com>
2021-11-22 23:38:43 +0900

    17_some.ts Update

--- 01_typescript/17_some.ts
+++ 01_typescript/17_some.ts
@@ -1,7 +1,7 @@
 const todos = [
   { id: 1, text: 'JavaScript', isDone: true },
-  { id: 2, text: 'NodeJS', isDone: true },
-  { id: 3, text: 'TypeScript', isDone: true },
+  { id: 2, text: 'TypeScript', isDone: true },
+  { id: 3, text: 'NodeJS', isDone: false },
   { id: 4, text: 'Express', isDone: false },
 ];
 
