4293f3432a64ca663b03f79ff64be2fc07adddda
t2 <oo_ri@naver.com>
2021-11-27 11:42:51 +0900

    35_all.ts Update

--- 01_typescript/35_all.ts
+++ 01_typescript/35_all.ts
@@ -2,17 +2,24 @@ type Resolve = (value: string) => void;
 
 const work = (ms: number) => {
   return new Promise((resolve: Resolve) => {
+    const date = new Date().toISOString();
+
     setTimeout(() => {
-      resolve(new Date().toISOString());
+      resolve(date);
     }, ms);
   });
 };
 
-const workFunc = async () => {
+const main = async () => {
+  console.time('걸린 시간');
+
   const [date1, date2] = await Promise.all([work(1000), work(2000)]);
 
   console.log(`첫 번째 작업: ${date1}`);
   console.log(`두 번째 작업: ${date2}`);
+
+  console.log();
+  console.timeEnd('걸린 시간');
 };
 
-workFunc();
+main();
