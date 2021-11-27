cfb4e1ee80363acd7f11672078c40ab37eb9ecb4
t2 <oo_ri@naver.com>
2021-11-27 11:16:04 +0900

    25_sce.ts Update

--- 01_typescript/25_sce.ts
+++ 01_typescript/25_sce.ts
@@ -6,7 +6,10 @@ console.log('TypeScript' && 'JavaScript');
 console.log('' && 'TypeScript');
 console.log(true && 'TypeScript');
 console.log(false && 'TypeScript');
+console.log(NaN && 'TypeScript');
 
+console.log();
+console.log('----------');
 console.log();
 
 console.log(undefined || 'TypeScript');
@@ -17,12 +20,18 @@ console.log('TypeScript' || 'JavaScript');
 console.log('' || 'TypeScript');
 console.log(true || 'TypeScript');
 console.log(false || 'TypeScript');
+console.log(NaN || 'TypeScript');
 
+console.log();
+console.log('----------');
 console.log();
 
 console.log(undefined ?? 'TypeScript');
 console.log(null ?? 'TypeScript');
+console.log(1 ?? 'TypeScript');
 console.log(0 ?? 'TypeScript');
+console.log('TypeScript' ?? 'JavaScript');
 console.log('' ?? 'TypeScript');
+console.log(true ?? 'TypeScript');
 console.log(false ?? 'TypeScript');
 console.log(NaN ?? 'TypeScript');
