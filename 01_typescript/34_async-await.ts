4bb991e6f11ecfea9d42450c8eca118746ea96d7
t2 <oo_ri@naver.com>
2021-11-27 11:39:08 +0900

    34_async-await.ts Update

--- 01_typescript/34_async-await.ts
+++ 01_typescript/34_async-await.ts
@@ -3,23 +3,25 @@ type Reject = (error: Error) => void;
 
 const work = (ms: number) => {
   return new Promise((resolve: Resolve, reject: Reject) => {
+    const date = new Date().toISOString();
+
     setTimeout(() => {
-      resolve(new Date().toISOString());
-      // reject(new Error('Error Message'));
+      resolve(date);
+      // reject(new Error(`에러 발생: ${date}`));
     }, ms);
   });
 };
 
-const workFunc = async () => {
+const main = async () => {
   try {
     const date1 = await work(1000);
-    const date2 = await work(1000);
-
     console.log(`첫 번째 작업: ${date1}`);
+
+    const date2 = await work(1000);
     console.log(`두 번째 작업: ${date2}`);
   } catch (e) {
     console.error((e as Error).message);
   }
 };
 
-workFunc();
+main();
