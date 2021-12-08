e3c29c0127504b6a22abd568fd8803365b228cd6
t2 <oo_ri@naver.com>
2021-12-09 00:12:36 +0900

    36_all.ts Update

--- 01_typescript/36_all.ts
+++ 01_typescript/36_all.ts
@@ -2,16 +2,21 @@ type Resolve = (value: string) => void;
 
 const work = (ms: number) => {
   return new Promise((resolve: Resolve) => {
-    const date = new Date().toISOString();
-
     setTimeout(() => {
-      resolve(date);
+      const now = new Date().toISOString();
+
+      resolve(now);
     }, ms);
   });
 };
 
 const main = async () => {
   console.time('걸린 시간');
+
+  const startTime = new Date().toISOString();
+
+  console.log(`시작 시간: ${startTime}`);
+  console.log();
 
   const [date1, date2] = await Promise.all([work(1000), work(2000)]);
 
