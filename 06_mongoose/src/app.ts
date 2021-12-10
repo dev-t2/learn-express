a1b5191e44a9ad9f520387cf5073b3345baef113
t2 <oo_ri@naver.com>
2021-12-11 01:48:03 +0900

    Mongoose Connection Update

--- 06_mongoose/src/app.ts
+++ 06_mongoose/src/app.ts
@@ -1,30 +1,39 @@
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 
-const app = express();
+const main = async () => {
+  try {
+    const app = express();
 
-mongoose.connect(process.env.URI);
+    await mongoose.connect(process.env.DB_URI!);
 
-app.set('port', 3000);
+    app.set('port', 3000);
 
-app.use(morgan('dev'));
+    app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('Hello Express');
-});
+    app.get('/', (req, res) => {
+      res.send('Hello Express');
+    });
 
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
+    app.use((req, res) => {
+      res.status(404).send('Not Found');
+    });
 
-// eslint-disable-next-line no-unused-vars
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
+    // eslint-disable-next-line no-unused-vars
+    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+      console.error(err);
 
-  res.status(500).send('Internal Server Error');
-});
+      res.status(500).send('Internal Server Error');
+    });
 
-app.listen(app.get('port'), () => {
-  console.log(`Server running at http://localhost:${app.get('port')}`);
-});
+    app.listen(app.get('port'), () => {
+      console.log(`Server running at http://localhost:${app.get('port')}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
