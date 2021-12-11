ef8af1831c2d98c4ce8e56bf8a73ee15b86a53bb
t2 <oo_ri@naver.com>
2021-12-11 15:32:35 +0900

    Mongoose User Model Update

--- 06_mongoose/src/app.ts
+++ 06_mongoose/src/app.ts
@@ -4,36 +4,36 @@ import mongoose from 'mongoose';
 import morgan from 'morgan';
 
 const main = async () => {
+  const app = express();
+
   try {
-    const app = express();
-
     await mongoose.connect(process.env.DB_URI!);
-
-    app.set('port', 3000);
-
-    app.use(morgan('dev'));
-
-    app.get('/', (req, res) => {
-      res.send('Hello Express');
-    });
-
-    app.use((req, res) => {
-      res.status(404).send('Not Found');
-    });
-
-    // eslint-disable-next-line no-unused-vars
-    app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      console.error(err);
-
-      res.status(500).send('Internal Server Error');
-    });
-
-    app.listen(app.get('port'), () => {
-      console.log(`Server running at http://localhost:${app.get('port')}`);
-    });
   } catch (err) {
     console.error(err);
   }
+
+  app.set('port', process.env.PORT ?? 3000);
+
+  app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+
+  app.get('/', (req, res) => {
+    res.send('Hello Express');
+  });
+
+  app.use((req, res) => {
+    res.status(404).send('Not Found');
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    res.status(500).send('Internal Server Error');
+  });
+
+  app.listen(app.get('port'), () => {
+    console.log(`Server running at http://localhost:${app.get('port')}`);
+  });
 };
 
 main();
---
+++ 06_mongoose/src/model/user.ts
@@ -0,0 +1,16 @@
+import { model, Schema } from 'mongoose';
+
+interface IUser {
+  email: string;
+  username: string;
+}
+
+const schema = new Schema<IUser>(
+  {
+    email: { type: String, required: true },
+    username: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const User = model<IUser>('User', schema);
