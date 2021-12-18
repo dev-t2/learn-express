c978377c3d69a1e761bc62ff63af1a2b252df247
t2 <oo_ri@naver.com>
2021-12-18 15:30:18 +0900

    04_static app.ts process.env Update

--- 04_static/src/app.ts
+++ 04_static/src/app.ts
@@ -4,9 +4,9 @@ import path from 'path';
 
 const app = express();
 
-app.set('port', 3000);
+app.set('port', process.env.PORT || 3000);
 
-app.use(morgan('dev'));
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
