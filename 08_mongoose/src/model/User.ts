eaba155fc3c9f80cc07ff5b57b5beb0c6d82b481
t2 <oo_ri@naver.com>
2021-12-31 23:56:23 +0900

    08_mongoose Update

--- 08_mongoose/src/model/User.ts
+++ 08_mongoose/src/model/User.ts
@@ -2,11 +2,13 @@ import { Document, model, Schema } from 'mongoose';
 
 interface IUser extends Document {
   email: string;
+  nickname: string;
 }
 
 const UserSchema = new Schema<IUser>(
   {
     email: { type: String, required: true, unique: true },
+    nickname: { type: String, required: true },
   },
   { timestamps: true }
 );
