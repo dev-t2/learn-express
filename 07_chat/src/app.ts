a7ebe32e97a5c75bac7f3d8ac55029c99a94efab
t2 <oo_ri@naver.com>
2022-01-02 17:06:11 +0900

    07_chat Setup

---
+++ 07_chat/src/app.ts
@@ -0,0 +1,45 @@
+import express, { NextFunction, Request, Response } from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import morgan from 'morgan';
+import path from 'path';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.set('port', 3000);
+
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  res.status(500).send('Internal Server Error');
+});
+
+io.on('connection', (socket) => {
+  console.log(`Connected User: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`Disconnected User: ${socket.id}`);
+  });
+
+  socket.on('error', (err) => {
+    console.error(err);
+  });
+
+  socket.on('message', (message) => {
+    io.emit('message', message);
+  });
+});
+
+server.listen(app.get('port'), () => {
+  console.log(`Server running at http://localhost:${app.get('port')}`);
+});
---
+++ 07_chat/src/public/script/app.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+
+const socket = io();
+
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (input.value) {
+    socket.emit('message', input.value);
+
+    input.value = '';
+  }
+});
+
+socket.on('message', (message) => {
+  console.log(message);
+});
