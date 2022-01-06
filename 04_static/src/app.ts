c89d8eac55a3f68fb28ac076cf32cc167acb0565
t2 <oo_ri@naver.com>
2022-01-07 00:07:29 +0900

    04_static Complete

--- 04_static/src/app.ts
+++ 04_static/src/app.ts
@@ -12,14 +12,14 @@ app.use(
 );
 
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  return res.status(404).send('Not Found');
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
-  res.status(500).send('Internal Server Error');
+  return res.status(500).send('Internal Server Error');
 });
 
 app.listen(app.get('port'), () => {
