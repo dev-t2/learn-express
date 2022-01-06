e468498debf95b2ef342b51ca67714f4b1401441
t2 <oo_ri@naver.com>
2022-01-07 00:08:37 +0900

    03_express Complete

--- 03_express/src/app.ts
+++ 03_express/src/app.ts
@@ -8,18 +8,18 @@ app.set('port', 3000);
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.get('/', (req, res) => {
-  res.send('Hello Express');
+  return res.send('Hello Express');
 });
 
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  return res.status(404).send('Not Found');
 });
 
 // eslint-disable-next-line no-unused-vars
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
-  res.status(500).send('Internal Server Error');
+  return res.status(500).send('Internal Server Error');
 });
 
 app.listen(app.get('port'), () => {
