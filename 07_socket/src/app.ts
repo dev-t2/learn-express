be7c36440d35f94b85f620afa9d8e0b8740b80ac
t2 <oo_ri@naver.com>
2022-02-02 11:44:33 +0900

    07_socket Reset and Update

--- 07_socket/src/app.ts
+++ 07_socket/src/app.ts
@@ -11,7 +11,9 @@ const io = new Server(server);
 app.set('port', 3000);
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  express.static(path.join(__dirname, 'public'), { extensions: ['html'] })
+);
 
 app.use((req, res) => {
   res.status(404).send('Not Found');
@@ -24,22 +26,10 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send('Internal Server Error');
 });
 
-io.on('connection', (socket) => {
-  console.log(`Connected User: ${socket.id}`);
-
-  socket.on('message', (message) => {
-    io.emit('message', message);
-  });
-
-  socket.on('error', (err) => {
-    console.error(err);
-  });
-
-  socket.on('disconnect', () => {
-    console.log(`Disconnected User: ${socket.id}`);
-  });
-});
-
 server.listen(app.get('port'), () => {
   console.log(`Server running at http://localhost:${app.get('port')}`);
+});
+
+io.on('connection', (socket) => {
+  console.log(socket);
 });
--- 07_socket/src/public/script/app.js
+++ 07_socket/src/public/script/app.js
@@ -2,17 +2,4 @@
 
 const socket = io();
 
-const form = document.querySelector('form');
-const input = form.querySelector('input');
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-
-  socket.emit('message', input.value);
-
-  input.value = '';
-});
-
-socket.on('message', (message) => {
-  console.log(message);
-});
+console.log(socket);
