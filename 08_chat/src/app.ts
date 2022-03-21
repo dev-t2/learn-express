e91f78d377b11159adbd34e968feb59d14782c31
t2 <oo_ri@naver.com>
2022-03-21 22:37:47 +0900

    404 and 500 Middleware Update

--- 08_chat/src/app.ts
+++ 08_chat/src/app.ts
@@ -15,13 +15,13 @@ app.use(
 );
 
 app.use((req, res) => {
-  res.status(404).send('Not Found');
+  return res.status(404).send('Not Found');
 });
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
-  res.status(500).send('Internal Server Error');
+  return res.status(500).send('Internal Server Error');
 });
 
 const server = createServer(app);
