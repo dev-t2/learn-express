d48b2d67ad1faa4221da15fdd7930ebf9a23cd27
dev-t2 <oo_ri@naver.com>
2023-11-21 04:02:23 +0900

    02. Express Update

--- 02-express/src/app.ts
+++ 02-express/src/app.ts
@@ -1,25 +1,24 @@
-import express, { NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
+import express from 'express';
 
 const app = express();
 
 app.set('port', 8080);
 
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+// app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.get('/', (req, res) => {
   return res.send('Hello Express');
 });
 
-app.use((req, res) => {
-  return res.status(404).send('Not Found');
-});
+// app.use((req, res) => {
+//   return res.status(404).send('Not Found');
+// });
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//   console.error(err);
 
-  return res.status(500).send('Internal Server Error');
-});
+//   return res.status(500).send('Internal Server Error');
+// });
 
 app.listen(app.get('port'), () => {
   console.log(`Server running at http://localhost:${app.get('port')}`);
