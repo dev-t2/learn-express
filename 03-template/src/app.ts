307853e2ce19cb8d44b3b512c5612c61b2b9b097
dev-t2 <oo_ri@naver.com>
2023-11-23 23:15:29 +0900

    03. Template Static Files Update

--- 03-template/src/app.ts
+++ 03-template/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
+import path from 'path';
 
 const app = express();
 
 const port = 8080;
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   return res.send('Hello Express');
