0a362be41b2c368021754a8c7426550d0466a25b
dev-t2 <oo_ri@naver.com>
2023-11-23 23:38:11 +0900

    Pug Install and Setup

--- 03-template/src/app.ts
+++ 03-template/src/app.ts
@@ -6,9 +6,17 @@ const app = express();
 
 const port = 8080;
 
+app.set('view engine', 'pug');
+
+app.set('views', './src/views');
+
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/template', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
 
 app.get('/', (req, res) => {
   return res.send('Hello Express');
