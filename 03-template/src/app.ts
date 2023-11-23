0b641c9371f7934ae20c5a9f73602a699b6eae75
dev-t2 <oo_ri@naver.com>
2023-11-23 23:10:56 +0900

    03. Template Install and Setup

--- 03-template/src/app.ts
+++ 03-template/src/app.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import morgan from 'morgan';
-import path from 'path';
 
 const app = express();
 
@@ -8,7 +7,9 @@ const port = 8080;
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  return res.send('Hello Express');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
