c47e52b7955438d6ff7d21ea3f5cf9816d334b22
dev-t2 <oo_ri@naver.com>
2023-11-25 09:47:51 +0900

    02. Express, 03. Static and 04. API Code Refactoring

--- 04-api/src/app.ts
+++ 04-api/src/app.ts
@@ -1,5 +1,12 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+
+interface IUser {
+  id: number;
+  name: string;
+}
+
+const users: IUser[] = [];
 
 const app = express();
 
@@ -7,8 +14,32 @@ const port = 8080;
 
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
-app.get('/', (req, res) => {
-  return res.send('Hello Express');
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  return res.json({ users });
+});
+
+interface IRequestCreateUser extends Request {
+  body: { name: string };
+}
+
+app.post('/users', (req: IRequestCreateUser, res) => {
+  return res.json();
+});
+
+app.put('/users', (req, res) => {});
+
+app.delete('/users', (req, res) => {});
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  return res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
