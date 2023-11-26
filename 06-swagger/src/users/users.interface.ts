affa467f74aea537f1b53c7d1a6d6dcc085321a1
dev-t2 <oo_ri@naver.com>
2023-11-26 19:31:16 +0900

    06. Swagger Setup

---
+++ 06-swagger/src/users/users.interface.ts
@@ -0,0 +1,18 @@
+import { Request } from 'express';
+
+export interface IRequestFindUser extends Request {
+  params: { id: string };
+}
+
+export interface IRequestCreateUser extends Request {
+  body: { nickname?: string };
+}
+
+export interface IRequestUpdateUser extends Request {
+  params: { id: string };
+  body: { nickname?: string };
+}
+
+export interface IRequestDeleteUser extends Request {
+  params: { id: string };
+}
