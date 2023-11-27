da72c3122a6f66bd792374756f3ce4c396e4d16e
dev-t2 <oo_ri@naver.com>
2023-11-27 21:00:10 +0900

    06. Todos Update

---
+++ 06-todos/src/app.ts
@@ -0,0 +1,24 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+const port = 8080;
+
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+
+app.use(express.json());
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  return res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
