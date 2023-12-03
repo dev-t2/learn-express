952a960b18e1b68020fccf5c1cf21851674f50d5
dev-t2 <oo_ri@naver.com>
2023-12-03 10:05:02 +0900

    03. Static Code Refactoring

--- 03-static/src/app.ts
+++ 03-static/src/app.ts
@@ -6,18 +6,18 @@ const app = express();
 
 const port = 8080;
 
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res) => {
-  return res.status(404).send('Not Found');
-});
+app.use(morgan('dev'));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
   return res.status(500).send('Internal Server Error');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
 });
 
 app.listen(port, () => {
