292e6b4c777d18a82d73215f990e58990e58dd1d
dev-t2 <oo_ri@naver.com>
2023-12-03 10:09:20 +0900

    06. Todos Code Refactoring

--- 06-todos/src/app.ts
+++ 06-todos/src/app.ts
@@ -4,26 +4,26 @@ import path from 'path';
 
 import TodosRouter from './todos/todos.router';
 
-const app = express();
-
 const port = 8080;
 
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan('dev'));
 
 app.use(express.json());
-
-app.use('/todos', TodosRouter);
-
-app.use((req, res) => {
-  return res.status(404).send('Not Found');
-});
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
 
   return res.status(500).send('Internal Server Error');
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/todos', TodosRouter);
+
+app.use((req, res) => {
+  return res.status(404).send('Not Found');
 });
 
 app.listen(port, () => {
