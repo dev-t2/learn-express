3d24899b2d06f8eb7a8f4fc952548f1cc68b182f
dev-t2 <oo_ri@naver.com>
2023-12-03 10:06:35 +0900

    04. API Code Refactoring

--- 04-api/src/app.ts
+++ 04-api/src/app.ts
@@ -12,9 +12,15 @@ const port = 8080;
 
 let users: IUser[] = [];
 
-app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(morgan('dev'));
 
 app.use(express.json());
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  return res.status(500).send('Internal Server Error');
+});
 
 app.get('/users', (req, res) => {
   return res.json({ users });
@@ -109,12 +115,6 @@ app.delete('/users/:id', (req: IRequestDeleteUser, res) => {
 
 app.use((req, res) => {
   return res.status(404).send('Not Found');
-});
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-
-  return res.status(500).send('Internal Server Error');
 });
 
 app.listen(port, () => {
