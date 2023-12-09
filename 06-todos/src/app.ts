d5dd75c78b067c28bf7e0d838d3e5a56a3e09010
dev-t2 <oo_ri@naver.com>
2023-12-09 17:41:32 +0900

    06. Todos Update

--- 06-todos/src/app.ts
+++ 06-todos/src/app.ts
@@ -8,15 +8,9 @@ const port = 8080;
 
 const app = express();
 
-app.use(morgan('dev'));
-
 app.use(express.json());
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-
-  return res.status(500).send('Internal Server Error');
-});
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,6 +20,12 @@ app.use((req, res) => {
   return res.status(404).send('Not Found');
 });
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  return res.status(500).send('Internal Server Error');
+});
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Listening and serving HTTP on :${port}`);
 });
